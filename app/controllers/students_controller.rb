de27fdcacbab56662cc3fdc31ce97098abca6c3f
Trac Nguyen <nguyendtrac@gmail.com>
2018-08-28 04:29:27 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -10,6 +10,9 @@ class StudentsController < ApplicationController
 
   def activate
     @student = Student.find(params[:id])
+    @student.active = !@student.active
+    @student.save
+    redirect_to student_path(@student)
   end
 
   private
